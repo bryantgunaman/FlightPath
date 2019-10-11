e38f9b94e5bc75e431a785007f16b4b3865628f8
bryantgunaman <bryantgunaman@gmail.com>
2019-10-11 15:32:56 -0700

    Initial Commit

---
+++ FlightPath/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlightPath
+//
+//  Created by Bryant Gunaman on 10/11/19.
+//  Copyright © 2019 Bryant Gunaman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
